e09bbbbcad910d53d076d477d6dd8df085ee816f
GregS83-HU <saussezg@icloud.com>
2025-08-01 06:31:08 +0200

    Ajout de Plasmic

---
+++ plasmic-init.js
@@ -0,0 +1,12 @@
+// plasmic-init.js
+import { initPlasmicLoader } from '@plasmicapp/loader-nextjs';
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: '7z3xcfGksCdvA9UNTDmBwf',
+      token: 'mP4AeNHKB6Y4gTkd0Ss25V0DA4s1EIQjn0gT96AYvlfsEEptgEiGRmtlRmkA3MB2LEAr6ACxNCKsnWvj5Og',
+    },
+  ],
+  preview: true,
+});
