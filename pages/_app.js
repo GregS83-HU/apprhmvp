8c2c89ee175a8e1536472d2bc15aadc2b2b959f1
GregS83-HU <saussezg@icloud.com>
2025-08-01 12:17:39 +0200

    Fix: ajout de plasmicrootprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { PlasmicRootProvider } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "../plasmic-init";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PlasmicRootProvider loader={PLASMIC}>
+      <Component {...pageProps} />
+    </PlasmicRootProvider>
+  );
 }
